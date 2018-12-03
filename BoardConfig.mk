89357ab0132411eedccbefce5ecae4fc181332e5
lupescu <expand33@gmail.com>
2018-12-02 17:08:01 -0800

    Create BoardConfig.mk

---
+++ BoardConfig.mk
@@ -0,0 +1,75 @@
+LOCAL_PATH := device/moto/jeter/
+
+TARGET_BOARD_VENDOR := motorola
+TARGET_BOARD_PLATFORM := msm8937
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno308
+
+# Encryption support
+TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd
+
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072			# (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216		# 16384 blocks
+BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456		# 262144 blocks
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432		# 32768 blocks
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16879616		# 16484 blocks
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2516582400		# 2457600 blocks
+# BOARD_USERDATAIMAGE_PARTITION_SIZE := 27124546560	# 26488815 blocks
+
+# File systems
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := msm8937
+#TARGET_BOOTLOADER_BOARD_NAME := jeter
+#TARGET_NO_BOOTLOADER := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 vmalloc=400M
+#BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 vmalloc=400M androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS :=  --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+TARGET_KERNEL_CONFIG := jeter_defconfig
+TARGET_KERNEL_SOURCE := kernel/moto/msm
+
+# Recovery
+TARGET_RECOVERY_FSTAB := device/moto/jeter/recovery/root/etc/twrp.fstab
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16879616
+BOARD_FLASH_BLOCK_SIZE := 0
+BOARD_HAS_NO_REAL_SDCARD := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_NO_MISC_PARTITION := false
+BOARD_RECOVERY_SWIPE := true
+BOARD_USES_MMCUTILS := true
+BOARD_SUPPRESS_EMMC_WIPE := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+TW_BRIGHTNESSS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+TW_DEFAULT_BRIGHTNESS := 149
+TW_MAX_BRIGHTNESS := 255
+TW_EXCLUDE_SUPERSU := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_INCLUDE_CRYPTO := true
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+
