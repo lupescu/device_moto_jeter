19f82d46eb94f5e5120ffd1be14406cf603ddd1d
lupescu <expand33@gmail.com>
2018-12-03 16:51:01 -0800

    Create vendor_prop.mk

---
+++ vendor_prop.mk
@@ -0,0 +1,12 @@
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.audio.calfile0=/vendor/etc/acdbdata/Bluetooth_cal.acdb \
+    persist.audio.calfile1=/vendor/etc/acdbdata/General_cal.acdb \
+    persist.audio.calfile2=/vendor/etc/acdbdata/Global_cal.acdb \
+    persist.audio.calfile4=/vendor/etc/acdbdata/Hdmi_cal.acdb \
+    persist.audio.calfile3=/vendor/etc/acdbdata/Handset_cal.acdb \
+    persist.audio.calfile5=/vendor/etc/acdbdata/Headset_cal.acdb \
+    persist.audio.calfile6=/vendor/etc/acdbdata/Speaker_cal.acdb    
+    
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.display.enable_default_color_mode=1 \
+    ro.sf.hwc_set_default_colormode=true
