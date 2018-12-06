9af1ae58f862603523f89fc7eb38e335db70e743
lupescu <expand33@gmail.com>
2018-12-06 04:59:24 -0800

    Update rr_jeter.mk

--- rr_jeter.mk
+++ rr_jeter.mk
@@ -1,26 +1,16 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+all inherit-product, device/motorola/addison/full_jeter.mk)
 PRODUCT_LOCALES := en_US
-
-# Screen density
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1440
-TARGET_SCREEN_WIDTH := 720
-
-# Fstab
-PRODUCT_COPY_FILES += \
-    device/moto/jeter/recovery/root/fstab.qcom:root/fstab.qcom \
-    device/moto/jeter/recovery/root/init.recovery.qcom.rc:root/init.recovery.qcom.rc
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := jeter
-PRODUCT_NAME := full_jeter
+PRODUCT_NAME := rr_jeter
 PRODUCT_BRAND := motorola
-PRODUCT_MODEL := g6_play
 PRODUCT_MANUFACTURER := motorola
 
-PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
-ro.product.model
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model
+ro.vendor.build.thumbprint=8.0.0/OCP27.91-150/165:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=motorola/jeter/jeter:8.0.0/OCP27.91-150/165:user/release-keys \
+    PRIVATE_BUILD_DESC="jeter-user 8.0.0 OCP27.91-150 165 release-keys" \
+    PRODUCT_NAME="moto g(6) play"
