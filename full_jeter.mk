9c98aa9af4ef4eeb7f00db772766fe5b3b53e461
lupescu <expand33@gmail.com>
2018-12-09 11:01:10 -0800

    Delete full_jeter.mk

--- full_jeter.mk
+++
@@ -1,7 +0,0 @@
-$(call inherit-product, device/motorola/jeter/device.mk)
-
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := jeter
-PRODUCT_NAME := full_jeter
-
-$(call inherit-product-if-exists, vendor/motorola/jeter/jeter-vendor.mk)
